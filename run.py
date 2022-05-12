45677c219615ee6c8f026980d4c765f0bb588f72
moni-que <monicahjustus@gmail.com>
2022-05-12 11:35:29 +0300

    call posts

--- run.py
+++ run.py
@@ -1,15 +1,30 @@
 from flask import Flask, render_template
 app = Flask(__name__)
 
+posts = [
+    {
+        'author':'monique',
+        'title':'Blog_post_1',
+        'content':'first_content',
+        'date_posted':'April 1 2022'
+    },
+    {
+        'author':'bambi',
+        'title':'Blog_post_2',
+        'content':'second_content',
+        'date_posted':'April 1 2021'
+    }
+]
+
 
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    return render_template('home.html', posts=posts)
 
 @app.route("/about")
 def about():
-    return "hello"
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
