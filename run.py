1c83206f9fdec69f0d128c347f59b549b920f338
moni-que <monicahjustus@gmail.com>
2022-05-13 07:06:57 +0300

    import SQLAlchemy and add database uri

--- run.py
+++ run.py
@@ -1,11 +1,16 @@
 # from turtle import title
 # from crypt import methods
+
 from flask import Flask, render_template,url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegisterForm, LoginForm
+
 app = Flask(__name__)
 
-
 app.config['SECRET_KEY'] = '92421f0ddd14c64997f6841932943a87'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
 
 posts = [
     {
