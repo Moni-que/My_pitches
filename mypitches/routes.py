cecd00488927f733a883767e8c55b70214aee365
moni-que <monicahjustus@gmail.com>
2022-05-14 10:25:28 +0300

    add login_required decorator

--- mypitches/routes.py
+++ mypitches/routes.py
@@ -2,7 +2,7 @@ from flask import render_template,url_for, flash, redirect
 from mypitches import app, db,bcrypt
 from mypitches.forms import RegisterForm, LoginForm
 from mypitches.models import User, Post
-from flask_login import login_user, current_user,logout_user
+from flask_login import login_user, current_user,logout_user,login_required
 
 
 posts = [
@@ -64,3 +64,8 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('home'))
+
+@app.route("/account")
+@login_required
+def account():
+    return render_template('account.html',title = 'Account')
