c2c1474558e1909e229f9b1859c62fa658033239
moni-que <monicahjustus@gmail.com>
2022-05-14 10:33:30 +0300

    import request for next page

--- mypitches/routes.py
+++ mypitches/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template,url_for, flash, redirect
+from flask import render_template,url_for, flash, redirect,request
 from mypitches import app, db,bcrypt
 from mypitches.forms import RegisterForm, LoginForm
 from mypitches.models import User, Post
@@ -54,7 +54,8 @@ def login():
         user = User.query.filter_by(email=form.email.data).first()
         if user and bcrypt.check_password_hash(user.password, form.password.data):
             login_user(user, remember = form.remember.data)
-            return redirect(url_for('home'))
+            next_page = request.args.get('next')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login unsuccessful.please check your email  or pssword', 'danger')
 
