34fd85e9bb2fff1b6ddcfbc715ae5382efa1058b
moni-que <monicahjustus@gmail.com>
2022-05-14 08:51:38 +0300

    change base uri and import Bcrypt

--- mypitches/__init__.py
+++ mypitches/__init__.py
@@ -1,13 +1,16 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
 
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = '92421f0ddd14c64997f6841932943a87'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://bambi:12345@localhost/pitches'
+
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
 
 
 from mypitches import routes
