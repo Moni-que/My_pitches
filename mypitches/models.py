45cda385a10fdd61998b688310090e0408121445
moni-que <monicahjustus@gmail.com>
2022-05-14 09:54:08 +0300

    import UserMixin

--- mypitches/models.py
+++ mypitches/models.py
@@ -1,8 +1,14 @@
 from datetime import datetime
-from mypitches import db
+from mypitches import db, login_manager
+from flask_login import UserMixin
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 
-class User(db.Model):
+
+class User(UserMixin, db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer,primary_key = True)
     username = db.Column(db.String(255),index = True)
