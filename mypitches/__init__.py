5499f5625e1ea2d8aa890afb4feda1189cc046db
moni-que <monicahjustus@gmail.com>
2022-05-14 09:55:05 +0300

    install flask_login

--- mypitches/__init__.py
+++ mypitches/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
 
 
 app = Flask(__name__)
@@ -11,6 +12,8 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://bambi:12345@local
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+
 
 
 from mypitches import routes
