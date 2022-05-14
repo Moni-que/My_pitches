9e2f8e4c471059930444af7ce01c59a85835d53a
moni-que <monicahjustus@gmail.com>
2022-05-14 12:27:05 +0300

    make if statements for validating email and usernmae

--- mypitches/forms.py
+++ mypitches/forms.py
@@ -1,8 +1,8 @@
-from xml.dom import ValidationErr
 from flask_wtf import FlaskForm
 from wtforms import StringField,PasswordField, SubmitField, BooleanField
 from wtforms.validators import InputRequired,Length,Email, EqualTo,ValidationError
 from mypitches.models import User
+from flask_login import current_user
 
 class RegisterForm(FlaskForm):
     username = StringField('Username', validaters = [InputRequired(),Length(min = 5, max = 20)])
@@ -28,3 +28,21 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators = [InputRequired()])
     remember_me = BooleanField('Remember me')
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validaters = [InputRequired(),Length(min = 5, max = 20)])
+    email = StringField('Email', validators = [InputRequired(), Email()])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            user = User.query.filter_by(username = username.data).first()
+            if user :
+                raise ValidationError('username unavailable')
+
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            user = User.query.filter_by(email = email.data).first()
+            if user :
+                raise ValidationError('email unavailable')
